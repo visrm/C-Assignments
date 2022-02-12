ea45108409f649f6c7f60830c8ddf97529184872
V <77574839+Yvel-tal42-86@users.noreply.github.com>
2022-02-12 18:52:45 +0530

    adjt:renamed

--- Sum-and-reverse_num.c
+++ Sum-and-reverse_num.c
@@ -3,20 +3,19 @@
 /*                                                                                             */                                                                    */
 /***********************************************************************************************/
 
-
-#include <stdio.h>
+#include<stdio.h>
 
 void main()
 {
-int number ,digit ,i ,sum=0 ,reverse_num=0;
-printf("enter no. to add");
-scanf("%d",&number);
+int num ,digit ,i ,sum=0 ,rev_num=0;
+printf("Enter the number\n");
+scanf("%d",&num);
 for(i=1;i<=10;i++) // For 10 digit only
    {
     digit = number%10;
-    number = number/10;
+    num = num/10;
     sum = sum+digit;  // For obtaining sum of digits 
-    reverse_num = reverse_num*10 + digit; //For reversing the digits 
+    rev_num = rev_num*10 + digit; //For reversing the digits 
    }
-printf("sum of given number is : %d\n reversed number is : %d",sum, reverse_num);
+printf("sum of given number is : %d\n reversed number is : %d\n",sum, rev_num);
 }
