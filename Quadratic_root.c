26ca3b6c327070d7bd47408fe78dfdf5b6871a21
V <77574839+visrm@users.noreply.github.com>
2022-03-26 10:15:31 +0530

    minor adjustments

--- Quadratic_root.c
+++ Quadratic_root.c
@@ -1,29 +1,26 @@
 /****************************************************************************************************/
-/*      PROGRAM TO FIND ROOTS OF A QUADRATIC EQUATION                                               */
-/*                                                                                                  */                                                                    
+/*      PROGRAM TO FIND ROOTS OF A QUADRATIC EQUATION                                               */                                                                            
 /****************************************************************************************************/
-
-#include<stdio.h>             /*header files*/
+#include<stdio.h>                            //header files*
+#include<conio.h>
 #include<math.h>
 
 void main()
 {
-    float a, b, c;                 /*Data type of variables a,b,c
-                                    for quadratic eqn ax²+bx+c=0 
-    /* Obtaining inputs from the user for a,b,c respectively*/
+    float a, b, c, d, D; 
+    clrscr();
+    //User input                
     printf("Input value of a :\n");
     scanf("%f", &a);
     printf("Input value of b :\n");
     scanf("%f", &b);
     printf("Input value of c :\n");
     scanf("%f", &c);
-
-    float d,D;
-    D = b*b - 4*a*c;         /*Determinant of the Quadratic Equation*/
+    D = b*b - 4*a*c;                         //Determinant of the Quadratic Equation
     d = sqrt(D);
 
     /*Checks for root of equation*/
-    while(a!=0 && b!=0)        /*while loop*/
+    while(a!=0 && b!=0)                      
     {
         if(D==0) { printf("Root of the eqn is : %f", (-b)/(2*a) );
                    break;
@@ -31,8 +28,9 @@ void main()
         if(D>0) { printf("Roots of the eqn are: %f %f",((-b)-(d))/(2*a),((-b)+(d))/(2*a));
                   break;
         }
-        else  printf("Root doesn't exist");
+        else { printf("Root doesn't exist");
               break;
-    }  /*end of loop*/
-
+        }
+    }  //end of loop
+    getch();
 }
