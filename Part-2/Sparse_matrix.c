46fbcb4e8f0ca3b87d11ac3ad2b796d6ba8e1969
visrm <Yveltal4286@gmail.com>
2022-09-27 22:29:59 +0530

    :heavy_plus_sign: Sparse_matrix.c
    A simple C program to implement sparse matrix .

--- Part-2/Sparse_matrix.c
+++ Part-2/Sparse_matrix.c
@@ -1,41 +1,44 @@
-/*********************************************************************************/
-/*                   PROGRAM  TO  IMPLEMENT  SPARSE  MATRIX                      */
-/*********************************************************************************/
-#include<stdio.h>
-#include<conio.h>
+/****************************************************************************************************/
+/*                     PROGRAM  TO  IMPLEMENT  SPARSE  MATRIX                                       */
+/****************************************************************************************************/
+#include <stdio.h>
+#include <conio.h>
 void main()
 {
-    int A[10][10],B[10][3],m,n,s=0,i,j;
+    int A[10][10], B[10][3], m, n, s = 0, i, j;
     clrscr();
     printf("\nEnter the order m x n of the sparse matrix\n");
-    scanf("%d%d",&m,&n);
+    // m = no. of rows, n = no. of columns
+    scanf("%d%d", &m, &n);
     printf("\nEnter the elements in the sparse matrix(mostly zeroes)\n");
-    for(i=0; i<m; i++)
+    for (i = 0; i < m; i++)
     {
-        for(j=0; j<n; j++)
+        for (j = 0; j < n; j++)
         {
-            printf("\n%d row and %d column: ",i+1,j+1);
-            scanf("%d",&A[i][j]);
+            printf("\n%d row and %d column: ", i + 1, j + 1);
+            scanf("%d", &A[i][j]);
         }
     }
     printf("The given matrix is:\n");
-    for(i=0; i<m; i++)
+    // For loop for transversing(displaying) the user-inputted matrix
+    for (i = 0; i < m; i++)
     {
-        for(j=0; j<n; j++)
+        for (j = 0; j < n; j++)
         {
-            printf("%d\t ",A[i][j]);
+            printf("%d\t ", A[i][j]);
         }
         printf("\n");
     }
-    for(i=0; i<m; i++)
+    // For loop for implementing sparse of matrix
+    for (i = 0; i < m; i++)
     {
-        for(j=0; j<n; j++)
+        for (j = 0; j < n; j++)
         {
-            if(A[i][j]!=0)
+            if (A[i][j] != 0)
             {
-                B[s][0]=A[i][j];
-                B[s][1]=i+1;
-                B[s][2]=j+1;
+                B[s][0] = A[i][j];
+                B[s][1] = i + 1;
+                B[s][2] = j + 1;
                 s++;
             }
         }
@@ -43,11 +46,12 @@ void main()
     printf("\nThe sparse matrix is given by\n");
     printf("\n");
     printf("\tvalue\trow\tcolumn\n\n");
-    for(i=0; i<s; i++)
+    // For loop for diplaying sparse of matrix
+    for (i = 0; i < s; i++)
     {
-        for(j=0; j<3; j++)
+        for (j = 0; j < 3; j++)
         {
-            printf("\t%d ",B[i][j]);
+            printf("\t%d ", B[i][j]);
         }
         printf("\n");
     }
